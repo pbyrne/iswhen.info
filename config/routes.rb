21d4769b1bf7efd815227212634a07ecf266413a
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-02-08 06:10:19 -0600

    Ensure that www.iswhen.info properly goes to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 IsWhen::Application.routes.draw do
+  match "", to: "events#index", constraints: { subdomain: 'www' }
   match "", to: "events#show", constraints: { subdomain: /.+/ }
 
   root to: "events#index"
--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -3,11 +3,15 @@ require "spec_helper"
 describe EventsController do
   describe "routing" do
 
-    it "routes to #show" do
+    it "routes subdomains to #show" do
       get("http://foo.example.com").should route_to("events#show")
     end
 
-    it "routes to #index" do
+    it "routes www subdomain to #index" do
+      get("http://www.example.com").should route_to("events#index")
+    end
+
+    it "routes no subdomain to #index" do
       get("http://example.com").should route_to("events#index")
     end
 
