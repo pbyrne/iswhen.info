e9bd158bffa458f0a257e0dbc151ff6379713a8a
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-02-07 18:47:00 -0600

    Add some basic validations to observance

--- app/models/observance.rb
+++ app/models/observance.rb
@@ -1,3 +1,6 @@
 class Observance < ActiveRecord::Base
   belongs_to :event
+
+  validates :start_at, :presence => true
+  validates :event_id, :presence => true
 end
--- spec/models/observance_spec.rb
+++ spec/models/observance_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Observance do
+  context "#valid?" do
+    it { should validate_presence_of(:start_at) }
+    it { should validate_presence_of(:event_id) }
+  end
+
+  it { should belong_to(:event) }
 end
