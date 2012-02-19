4eeeb7f6207514561fb66a565416e422910d32f7
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-02-19 07:45:23 -0600

    Order the events by their upcoming date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.order_by_upcoming
 
     respond_to do |format|
       format.html # index.html.erb
