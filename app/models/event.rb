04e60ada79dceafcb06a69123fa77cdd16e387a9
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-22 09:19:40 -0600

    flesh out the validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
+  validates :longname,
+    :presence => true
+  validates :name,
+    :presence => true
   validates :shortname,
     :format => /^\w+$/,
     :uniqueness => true
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Event do
   context "#valid?" do
+    let(:event) { FactoryGirl.create(:event) }
+
     context "on shortname" do
-      let(:orig) { FactoryGirl.create(:event) }
-      let(:duplicate) { FactoryGirl.build(:event, shortname: orig.shortname) }
+      let(:duplicate) { FactoryGirl.build(:event, shortname: event.shortname) }
       let(:unique) { FactoryGirl.build(:event, shortname: "somethingelseentirely") }
 
       it "must be unique" do
@@ -16,23 +17,47 @@ describe Event do
       end
 
       it "must be a single word" do
-        orig.shortname = "foo"
-        orig.valid?
-        orig.errors[:shortname].should be_blank
+        event.shortname = "foo"
+        event.valid?
+        event.errors[:shortname].should be_blank
 
-        orig.shortname = "this is not right"
-        orig.valid?
-        orig.errors[:shortname].should_not be_blank
+        event.shortname = "this is not right"
+        event.valid?
+        event.errors[:shortname].should_not be_blank
       end
 
       it "cannot be blank" do
-        orig.shortname = "foo"
-        orig.valid?
-        orig.errors[:shortname].should be_blank
+        event.shortname = "foo"
+        event.valid?
+        event.errors[:shortname].should be_blank
 
-        orig.shortname = nil
-        orig.valid?
-        orig.errors[:shortname].should_not be_blank
+        event.shortname = nil
+        event.valid?
+        event.errors[:shortname].should_not be_blank
+      end
+    end
+
+    context "on longname" do
+      it "cannot be blank" do
+        event.longname = "foo"
+        event.valid?
+        event.errors[:longname].should be_blank
+
+        event.longname = nil
+        event.valid?
+        event.errors[:longname].should_not be_blank
+      end
+    end
+
+    context "on name" do
+      it "cannot be blank" do
+        event.name = "Something"
+        event.valid?
+        event.errors[:name].should be_blank
+
+        event.name = nil
+        event.valid?
+        event.errors[:name].should_not be_blank
       end
     end
   end
