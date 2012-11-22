6540c142e32c09a7d02ea7b52c7bfeca9e90062d
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-22 09:41:20 -0600

    give better failure messages in event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,31 +9,33 @@ describe Event do
       let(:unique) { FactoryGirl.build(:event, shortname: "somethingelseentirely") }
 
       it "must be unique" do
+        event.save
+
         duplicate.valid?
-        duplicate.errors[:shortname].should_not be_blank
+        duplicate.errors.should include :shortname
 
         unique.valid?
-        unique.errors[:shortname].should be_blank
+        unique.errors.should_not include :shortname
       end
 
       it "must be a single word" do
         event.shortname = "foo"
         event.valid?
-        event.errors[:shortname].should be_blank
+        event.errors.should_not include :shortname
 
         event.shortname = "this is not right"
         event.valid?
-        event.errors[:shortname].should_not be_blank
+        event.errors.should include :shortname
       end
 
       it "cannot be blank" do
         event.shortname = "foo"
         event.valid?
-        event.errors[:shortname].should be_blank
+        event.errors.should_not include :shortname
 
         event.shortname = nil
         event.valid?
-        event.errors[:shortname].should_not be_blank
+        event.errors.should include :shortname
       end
     end
 
@@ -41,11 +43,11 @@ describe Event do
       it "cannot be blank" do
         event.longname = "foo"
         event.valid?
-        event.errors[:longname].should be_blank
+        event.errors.should_not include :longname
 
         event.longname = nil
         event.valid?
-        event.errors[:longname].should_not be_blank
+        event.errors.should include :longname
       end
     end
 
@@ -53,11 +55,11 @@ describe Event do
       it "cannot be blank" do
         event.name = "Something"
         event.valid?
-        event.errors[:name].should be_blank
+        event.errors.should_not include :name
 
         event.name = nil
         event.valid?
-        event.errors[:name].should_not be_blank
+        event.errors.should include :name
       end
     end
   end
