32e4539332318de5d922b10ef45dcaf1a43c1318
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-22 06:42:10 -0600

    customize spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,8 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
