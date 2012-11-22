43b3ffd753b906516871e9bc72ce7a8d00ae16bb
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-22 06:37:36 -0600

    bare rails 3.2.9 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IswhenInfo::Application.config.secret_token = '34d8f4b30bbbfdd9a10c14bbac3234e0c4b469fa1047be8974c3d73d7fbd560e2b9fabaf06d025da497cb7622f1586281067861014b37ed9faf033dc3d83c4e8'
