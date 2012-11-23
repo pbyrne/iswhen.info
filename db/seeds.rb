c5c356694de69403dd9bdea1061f5baa29c19a4d
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-23 07:36:33 -0600

    add some more seeded events

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'event_loader'
 
 # sources
 # equinoxes and solstices: http://www.timeanddate.com/calendar/seasons.html
+# canadian thanksgiving: http://www.holidays.net/thanksgiving/canadadates.htm
 
 Event.destroy_all
 Observance.destroy_all
@@ -61,5 +62,62 @@ EventLoader.new("Summer Solstice", "summersolstice", "Summer Solstice (First Day
   loader.add "2015-06-21"
   loader.add "2016-06-20"
   loader.add "2017-06-21"
+end
+
+EventLoader.new("Christmas", "christmas", "Christmas").tap do |loader|
+  loader.add "2012-12-25"
+  loader.add "2013-12-25"
+  loader.add "2014-12-25"
+  loader.add "2015-12-25"
+  loader.add "2016-12-25"
+  loader.add "2017-12-25"
+  loader.add "2018-12-25"
+  loader.add "2019-12-25"
+  loader.add "2020-12-25"
+end
+
+EventLoader.new("Fourth of July", "fourthofjuly", "Fourth of July").tap do |loader|
+  loader.add "2013-07-04"
+  loader.add "2014-07-04"
+  loader.add "2015-07-04"
+  loader.add "2016-07-04"
+  loader.add "2017-07-04"
+  loader.add "2018-07-04"
+  loader.add "2019-07-04"
+  loader.add "2020-07-04"
+end
+
+EventLoader.new("Veterans Day", "veteransday", "Veterans Day").tap do |loader|
+  loader.add "2013-11-11"
+  loader.add "2014-11-11"
+  loader.add "2015-11-11"
+  loader.add "2016-11-11"
+  loader.add "2017-11-10"
+  loader.add "2018-11-12"
+  loader.add "2019-11-11"
+  loader.add "2020-11-11"
+end
+
+EventLoader.new("Can. Thanksgiving", "canadianthanksgiving", "Canadian Thanksgiving").tap do |loader|
+  loader.add "2013-10-14"
+  loader.add "2014-10-13"
+  loader.add "2015-10-12"
+  loader.add "2016-10-10"
+  loader.add "2017-10-09"
+  loader.add "2018-10-08"
+  loader.add "2019-10-14"
+  loader.add "2020-10-12"
+end
+
+EventLoader.new("Columbus Day", "columbusday", "Columbus Day").tap do |loader|
+  loader.add "2012-10-08"
+  loader.add "2013-10-14"
+  loader.add "2014-10-13"
+  loader.add "2015-10-12"
+  loader.add "2016-10-10"
+  loader.add "2017-10-09"
+  loader.add "2018-10-08"
+  loader.add "2019-10-14"
+  loader.add "2020-10-12"
 end
 
