5e37409db1f7cb32e22684bd802fd700ee985bce
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-11-24 06:51:58 -0600

    fill in the last of the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,12 @@ require 'event_loader'
 # sources
 # equinoxes and solstices: http://www.timeanddate.com/calendar/seasons.html
 # canadian thanksgiving: http://www.holidays.net/thanksgiving/canadadates.htm
-# far-future american holidays: http://www.when-is.com/martin-luther-king.asp
+# far-future american holidays: http://www.when-is.com/
 # far-future mardi gras: http://www.neworleansonline.com/neworleans/mardigras/mgdates.html
+# far-future easter: http://www.wheniseastersunday.com/year/2019/
+# far-future election day: http://www.when-is.com/election-day-2020.asp
+# far-future grandparents day: http://www.calendar-365.com/holidays/grandparents'-day.html
+# far-future daylight saving time: http://www.itronmeters.com/DST_Dates.htm
 
 Event.destroy_all
 Observance.destroy_all
@@ -158,6 +162,18 @@ EventLoader.new("Mother’s Day", "mothersday", "Mother’s Day").tap do |loader
   loader.add "2020-05-10"
 end
 
+EventLoader.new("Father’s Day", "fathersday", "Father’s Day").tap do |loader|
+  loader.add "2012-06-17"
+  loader.add "2013-06-16"
+  loader.add "2014-06-15"
+  loader.add "2015-06-21"
+  loader.add "2016-06-19"
+  loader.add "2017-06-18"
+  loader.add "2018-06-17"
+  loader.add "2019-06-16"
+  loader.add "2020-06-21"
+end
+
 EventLoader.new("Super Bowl", "superbowl", "Super Bowl").tap do |loader|
   loader.add "2013-02-03"
   loader.add "2014-02-02"
@@ -249,5 +265,112 @@ EventLoader.new("Mardi Gras", "mardigras", "Mardi Gras").tap do |loader|
   loader.add "2018-02-13"
   loader.add "2019-04-05"
   loader.add "2020-02-25"
+end
+
+EventLoader.new("Memorial Day", "memorialday", "Memorial Day").tap do |loader|
+  loader.add "2012-05-28"
+  loader.add "2013-05-27"
+  loader.add "2014-05-26"
+  loader.add "2015-05-25"
+  loader.add "2016-05-30"
+  loader.add "2017-05-29"
+  loader.add "2018-05-28"
+  loader.add "2019-05-25"
+  loader.add "2020-05-25"
+end
+
+EventLoader.new("Labor Day", "laborday", "Labor Day").tap do |loader|
+  loader.add "2012-09-03"
+  loader.add "2013-09-02"
+  loader.add "2014-09-01"
+  loader.add "2015-09-07"
+  loader.add "2016-09-05"
+  loader.add "2017-09-04"
+  loader.add "2018-09-03"
+  loader.add "2019-09-02"
+  loader.add "2020-09-07"
+end
+
+EventLoader.new("Halloween", "halloween", "Halloween").tap do |loader|
+  loader.add "2012-10-31"
+  loader.add "2013-10-31"
+  loader.add "2014-10-31"
+  loader.add "2015-10-31"
+  loader.add "2016-10-31"
+  loader.add "2017-10-31"
+  loader.add "2018-10-31"
+  loader.add "2019-10-31"
+  loader.add "2020-10-31"
+end
+
+EventLoader.new("Groundhog Day", "groundhogday", "Groundhog Day").tap do |loader|
+  loader.add "2012-02-02"
+  loader.add "2013-02-02"
+  loader.add "2014-02-02"
+  loader.add "2015-02-02"
+  loader.add "2016-02-02"
+  loader.add "2017-02-02"
+  loader.add "2018-02-02"
+  loader.add "2019-02-02"
+end
+
+EventLoader.new("DST End", "dstend", "End of Daylight Saving Time").tap do |loader|
+  loader.add "2012-11-04"
+  loader.add "2013-11-03"
+  loader.add "2014-11-02"
+  loader.add "2015-11-01"
+  loader.add "2016-11-06"
+  loader.add "2017-11-05"
+  loader.add "2018-11-04"
+  loader.add "2019-11-03"
+  loader.add "2020-11-01"
+end
+
+EventLoader.new("DST Start", "dststart", "Start of Daylight Saving Time").tap do |loader|
+  loader.add "2012-03-11"
+  loader.add "2013-03-10"
+  loader.add "2014-03-09"
+  loader.add "2015-03-08"
+  loader.add "2016-03-13"
+  loader.add "2017-03-12"
+  loader.add "2018-03-08"
+  loader.add "2019-03-10"
+  loader.add "2020-03-08"
+end
+
+EventLoader.new("Easter", "easter", "Easter").tap do |loader|
+  loader.add "2012-04-08"
+  loader.add "2013-03-31"
+  loader.add "2014-04-20"
+  loader.add "2015-04-05"
+  loader.add "2016-03-27"
+  loader.add "2017-04-16"
+  loader.add "2018-04-01"
+  loader.add "2019-04-21"
+  loader.add "2020-04-12"
+end
+
+EventLoader.new("US Election Day", "electionday", "US Election Day").tap do |loader|
+  loader.add "2012-11-06"
+  loader.add "2013-11-05"
+  loader.add "2014-11-04"
+  loader.add "2015-11-03"
+  loader.add "2016-11-08"
+  loader.add "2017-11-07"
+  loader.add "2018-11-06"
+  loader.add "2019-11-05"
+  loader.add "2020-11-03"
+end
+
+EventLoader.new("Grandparents Day", "grandparentsday", "Grandparents Day").tap do |loader|
+  loader.add "2012-09-09"
+  loader.add "2013-09-08"
+  loader.add "2014-09-07"
+  loader.add "2015-09-13"
+  loader.add "2016-09-11"
+  loader.add "2017-09-10"
+  loader.add "2018-09-09"
+  loader.add "2019-09-08"
+  loader.add "2020-09-13"
 end
 
