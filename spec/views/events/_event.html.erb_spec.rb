538b99a8175db46a7e9aa087a7fe033fbaecfbe6
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-17 08:56:13 -0600

    render events#show partials

---
+++ spec/views/events/_event.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe "events/_event.html.erb" do
+  pending
+end
--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe "events/show.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    assign(:collection, [])
+    assign(:event, FactoryGirl.build(:event) )
+  end
+
+  it "renders the selected event" do
+    render
+
+    view.should render_template(partial: "_event")
+  end
+
+  it "renders the list of upcoming events" do
+    render
+    view.should render_template(partial: "_upcoming_events")
+  end
 end
