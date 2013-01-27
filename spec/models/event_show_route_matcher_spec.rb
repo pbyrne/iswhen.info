9117bdf6a06f0eaa4336063026e2761499427cca
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-01-26 21:28:16 -0600

    Add EventShowRouteMatcher route constraint helper class

---
+++ spec/models/event_show_route_matcher_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe EventShowRouteMatcher do
+  let(:request) { stub(:request, subdomain: "foo") }
+
+  context "#matches?(request)" do
+    it "matches if given a subdomain matching an existing event" do
+      Event.stub(:for_subdomain).with(request.subdomain).and_return(FactoryGirl.build(:event))
+      subject.matches?(request).should be_true
+    end
+
+    it "does not match if subdomain does not match an event" do
+      Event.stub(:for_subdomain).with(request.subdomain).and_raise(Event::EventNotFound)
+      subject.matches?(request).should be_false
+    end
+  end
+end
