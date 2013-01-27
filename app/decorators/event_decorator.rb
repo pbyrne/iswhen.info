6d63d9bbd979b2615c7cfcb85e6c6af0d7b32a8b
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-01-27 16:08:02 -0600

    start flesing out the decorator

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,3 +1,18 @@
 class EventDecorator < Draper::Decorator
+  delegate :starts_sundown, :next_observance
 
+  # Public: Whether the event has upcoming observances
+  #
+  # Returns a Boolean
+  def has_upcoming_observances?
+    source.upcoming_observances.any?
+  end
+
+  # Public: The name of the day of week of its next observance
+  #
+  # Returns a String
+  def day_of_week
+    return "" if next_observance.nil?
+    next_observance.start_on.strftime("%A")
+  end
 end
--- spec/decorators/event_decorator_spec.rb
+++ spec/decorators/event_decorator_spec.rb
@@ -2,14 +2,47 @@ require "spec_helper"
 
 describe EventDecorator do
   let(:event) { FactoryGirl.build(:event) }
+  let(:observance) { FactoryGirl.build(:tomorrow) }
 
-  # subject { EventDecorator.new(event) }
+  subject { EventDecorator.new(event) }
 
-  it "properly infers the decorator" do
-    event.decorate.should be_decorated_with EventDecorator
+  context "becoming familiar with decorators" do
+    it "properly infers the decorator" do
+      event.decorate.should be_decorated_with EventDecorator
+    end
+
+    it "properly decorates by hand" do
+      EventDecorator.new(event).should be_decorated_with EventDecorator
+    end
   end
 
-  it "properly decorates by hand" do
-    EventDecorator.new(event).should be_decorated_with EventDecorator
+  context "#has_upcoming_observances?" do
+    let(:observance) { FactoryGirl.build(:observance) }
+
+    it "is true if the event has upcoming observances" do
+      event.stub(:upcoming_observances) { [observance] }
+      subject.should have_upcoming_observances
+    end
+
+    it "is false if the event has no upcoming observances" do
+      event.stub(:upcoming_observances) { [] }
+      subject.should_not have_upcoming_observances
+    end
+  end
+
+  context "#year_string" do
+    pending
+  end
+
+  context "#day_of_week" do
+    it "displays the full name of the day of the week of its next observance" do
+      event.stub(:next_observance) { observance }
+      subject.day_of_week.should == event.next_observance.start_on.strftime("%A")
+    end
+
+    it "properly handles an event with no next observance" do
+      event.stub(:next_observance) { nil }
+      subject.day_of_week.should == ""
+    end
   end
 end
