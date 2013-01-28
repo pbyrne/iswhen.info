501f5e3c0dad587b02a32748605300b6082af605
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-01-27 20:42:53 -0600

    add some pending specs

--- spec/views/events/_event.html.erb_spec.rb
+++ spec/views/events/_event.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
 
 describe "events/_event.html.erb" do
-  pending
+  let(:event) { FactoryGirl.build(:event) }
+
+  it "displays the long name of the event"
+  it "displays the day of week it occurs"
+  it "displays the date it occurs"
+
+  context "knowing future observances" do
+    it "includes some details of the observance after the upcoming"
+    it "says nothing if no event after the next"
+  end
 end
