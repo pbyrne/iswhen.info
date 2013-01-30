73873827f278f4603b42ca14b1c1ae0717cfdd4e
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-01-29 18:33:26 -0600

    start first view spec

--- spec/views/events/_event.html.erb_spec.rb
+++ spec/views/events/_event.html.erb_spec.rb
@@ -1,11 +1,20 @@
 require "spec_helper"
 
 describe "events/_event.html.erb" do
-  let(:event) { FactoryGirl.build(:event) }
+  let(:event) { FactoryGirl.build(:event).decorate }
 
-  it "displays the long name of the event"
+  before do
+    assign(:event, event)
+  end
+
+  it "displays the long name of the event" do
+    render
+    rendered.should include event.name
+  end
+
   it "displays the day of week it occurs"
   it "displays the date it occurs"
+  it "displays the year it occurs"
 
   context "knowing future observances" do
     it "includes some details of the observance after the upcoming"
