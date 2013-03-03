8545b561b1d043eed1d89df192cb9dfdb59e918b
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-03 16:21:10 -0600

    add empty XML API controller

---
+++ spec/routing/xml_routing_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe "routing for legacy XML API" do
+  it "routes to xml#index" do
+    expect(get: "/xml-api/?show=complete").to route_to({
+      controller: "xml_api",
+      action: "index"
+    })
+  end
+end
